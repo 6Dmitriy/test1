76582e1204d3bf36ade7dd751d9f4ec4bfc4d7f6
User Dimas <Dimuch344@gmail.com>
2023-01-23 21:35:00 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int value1 = 10;
         int value2 = 15;
-        int result = value1 + value2;
+        int value3 = 10;
+        int result = value1 + value2 + value3;
         System.out.println(result);
     }
 }
