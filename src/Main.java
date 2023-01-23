82a45fce6be91274f93ca34cc9e29d363f36dddc
6Dmitriy <121396098+6dmitriy@users.noreply.github.com>
2023-01-23 21:39:49 +0300

    test1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int value1 = 10;
         int value2 = 15;
-        int value3 = 10;
-        int result = value1 + value2 + value3;
+        int result = value1 + value2;
         System.out.println(result);
     }
 }
