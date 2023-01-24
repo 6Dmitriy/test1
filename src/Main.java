1f4be218cbd1922335e5228bc267c00df77b9885
User Dimas <Dimuch344@gmail.com>
2023-01-24 17:03:45 +0300

    All done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int value1 = 10;
         int value2 = 15;
-        int value3 = 10;
+        int value3 = 1;
         int result = value1 + value2 + value3;
         System.out.println(result);
     }
